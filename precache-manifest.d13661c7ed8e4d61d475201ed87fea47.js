555a1411b11df5859af101f412aac27da7fa13c7
Allan.W.C <allan_195@hotmail.com>
2019-06-30 19:08:09 +1000

    Updates

---
+++ precache-manifest.d13661c7ed8e4d61d475201ed87fea47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "121b206a02e3ddf9d5e349a3933a48ae",
+    "url": "/real-universe/index.html"
+  },
+  {
+    "revision": "d58e1c250e958b61a0b4",
+    "url": "/real-universe/static/css/main.cc9d8f33.chunk.css"
+  },
+  {
+    "revision": "ce12bb565c0b21da47da",
+    "url": "/real-universe/static/js/2.2cbba62f.chunk.js"
+  },
+  {
+    "revision": "d58e1c250e958b61a0b4",
+    "url": "/real-universe/static/js/main.dd35c39c.chunk.js"
+  },
+  {
+    "revision": "a358ca514ff34b347d0a",
+    "url": "/real-universe/static/js/runtime~main.495a0658.js"
+  },
+  {
+    "revision": "b80b9c653c5f77b0ad11b32db5b051ee",
+    "url": "/real-universe/static/media/sun-earth-shadow.b80b9c65.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/repository-name/precache-manifest.30e4a621575fbfce5d60e557700e5adb.js"
+  "/real-universe/precache-manifest.d13661c7ed8e4d61d475201ed87fea47.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/repository-name/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/real-universe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
